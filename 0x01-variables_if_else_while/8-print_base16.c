87664e717e0821f9755461708767a53655595480
Ehab Rafat <ehab.rafat77@gmail.com>
2023-03-17 13:18:58 -0700

    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-int i;
-for (i = 0; i < 9; ++i)
+char i;
+for (i = '0'; i <= '9'; i++)
 {
 putchar(i);
 }
-for (i = 'a'; i <= 'f'; ++i)
+for (i = 'a'; i <= 'f'; i++)
 {
 putchar(i);
 }
